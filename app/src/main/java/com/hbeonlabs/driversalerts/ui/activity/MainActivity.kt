7a3c72079522a3d6259d1f0ebf155d87aa749d81
Karman <karmansingharora01@gmail.com>
2023-03-30 08:03:54 +0530

    Initial

--- app/src/main/java/com/hbeonlabs/driversalerts/ui/activity/MainActivity.kt
+++ app/src/main/java/com/hbeonlabs/driversalerts/ui/activity/MainActivity.kt
@@ -1,31 +1,17 @@
 package com.hbeonlabs.driversalerts.ui.activity
 
-import android.content.Intent
-import android.view.Menu
-import android.view.MenuItem
-import androidx.fragment.app.Fragment
+import androidx.appcompat.widget.Toolbar
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.hbeonlabs.driversalerts.R
 import com.hbeonlabs.driversalerts.databinding.ActivityMainBinding
 import com.hbeonlabs.driversalerts.ui.base.BaseActivity
-import com.hbeonlabs.driversalerts.ui.fragment.camera.CameraFragment
-import com.hbeonlabs.driversalerts.ui.fragment.history.HistoryFragment
-import com.hbeonlabs.driversalerts.ui.fragment.notification.NotificationsFragment
-import com.hbeonlabs.driversalerts.ui.fragment.settings.SettingsFragment
 import dagger.hilt.android.AndroidEntryPoint
-import androidx.appcompat.widget.Toolbar
+
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>() {
     private lateinit var navController: NavController
-    /*companion object {
-        init {
-            System.loadLibrary("jingle_peerconnection_so")
-        }
-    }*/
-
-//    private lateinit var myToolbar: Toolbar
 
     override fun getLayoutResourceId(): Int {
 
@@ -35,12 +21,12 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     override fun initView() {
         super.initView()
 
-        // ======== Connecting Nav graph to Fragment
         val navHostFrag =
             supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
         navController = navHostFrag.navController
-//        myToolbar = findViewById(R.id.custom_toolbar);
 
+        val toolbar = findViewById<Toolbar>(R.id.custom_toolbar)
+        setSupportActionBar(toolbar)
 
         binding.bottomNav.setOnItemSelectedListener {
 
